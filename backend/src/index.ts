9517ca8d656a05f305c462435fb68ac581f2d309
vansh-choudhary01 <choudharyvansh642@gmail.com>
2026-09-19 10:42:36 +0530

    update cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -21,8 +21,11 @@ app.get("/health", (_req: Request, res: Response) => {
 })
 
 app.use(cors({
-    "origin": process.env.FRONTEND_URL || "http://localhost:5173"
-}))
+    origin: [process.env.FRONTEND_URL || 'http://localhost:5173'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(express.json());
 
 app.use("/api", userRouter);
